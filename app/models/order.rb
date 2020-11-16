9d308b170c24e90f6a68e8185691ea1bb914ba2b
julien garcia <ju.garcia38@yahoo.fr>
2020-11-16 18:36:32 +0100

    creation controllers

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :product
   belongs_to :user
+  validates :price, presence: true, trough: product
+  validates :quantity, presence: true
+  validates :total, presence: true
+  validates :status, presence: true
 end
