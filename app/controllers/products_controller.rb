ddee828cc89bb227bac224ddd089f8570817a44b
Hugo Kasbach <hugo.kasbach@protonmail.com>
2020-11-18 16:50:34 +0100

    Cloudinary fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,6 +43,6 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :category, :price)
+    params.require(:product).permit(:name, :category, :price, :photo)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
   belongs_to :farmer, class_name: 'User'
   has_many :orders, dependent: :destroy
+  has_one_attached :photo
 end
