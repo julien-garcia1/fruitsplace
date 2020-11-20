78c07ad5b5ebb6d5dcef95da1adc2a940426cfa8
Hugo Kasbach <hugo.kasbach@protonmail.com>
2020-11-20 11:37:52 +0100

    panier indent

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.where(user_id:current_user.id, status: "unpayed")
+    @orders = Order.where(user_id:current_user.id, status: "à payer")
   end
 
   def destroy
@@ -12,14 +12,14 @@ class OrdersController < ApplicationController
   def create
   @product = Product.find(params[:product_id])
   @user = current_user
-  @order = Order.new(product: @product, user: @user, status: 'unpayed')
+  @order = Order.new(product: @product, user: @user, status: 'à payer')
   @order.save
   redirect_to products_path, notice: 'Votre produit a bien été ajouté'
   end
 
   def pay
-    @orders = Order.where(user_id:current_user.id, status: "unpayed")
-    @orders.update_all(status: 'paid')
+    @orders = Order.where(user_id:current_user.id, status: "à payer")
+    @orders.update_all(status: 'payé')
     redirect_to root_path, notice:'Votre commande a bien été payé'
   end
 
